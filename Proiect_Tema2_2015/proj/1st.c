0c56c086b4d86802e8b70ea2b39c5b58478bf2d6
DenisTDR2 <denis.tdr@gmail.com>
2015-05-15 16:36:52 +0300

    3th

--- Proiect_Tema2_2015/proj/1st.c
+++ Proiect_Tema2_2015/proj/1st.c
@@ -17,13 +17,16 @@ void frecventa(int *fv, int L, int n, Film *film);
 void reRank(Film * f, int n, int vTot);
 int cmpFilm(const void * pf1,const  void * pf2);
 void scrierePuncte(char *out, Film *film, int n);
-double *VectRep(double *pr, int n);
-int SimulZipf(int n, double *F);
+void calcVectRep(double *pr, Film *film, int n, double **pFt, double **pFexp);
+int simulZipf(int n, double *F);
+void simulareVizualizari(double *F, int n, int k);
 void readInputFile(char *fileName, Film *film, int n, int *vTot);
+void writeDebug(Film *film, int n);
 
 int main(){
 	Film *film;
-	int n = 200, vTot;
+	int n = 200, vTot, k;
+	double alpha, *prt, *Ft, *Fexp;
 	film = (Film*)allocArrayMemory(sizeof(Film), n);
 	//citirea
 	readInputFile("DateFilme.txt", film, n, &vTot);
@@ -35,7 +38,24 @@ int main(){
 	writeDebug(film, n);
 
 	scrierePuncte("NumeLeader2.txt", film, n);
+	//partea de pana aici calculeaza alpha;
+	alpha = 0.897675610478;
 
+	prt = Zipf(alpha, n);
+
+	writeOutputFiles(film, n, prt);
+
+	calcVectRep(prt, film, n, &Ft, &Fexp);
+
+	for(k=0;(k<n) && (Ft[k]<0.75);k++);// ";" nu e din greasala!!!!!!!!!
+	
+	printf("\nNumarul de file salvate in cache este k = %d\n", k);
+
+	simulareVizualizari(Ft, n, k);
+	simulareVizualizari(Fexp, n, k);
+
+	if(prt)
+		free(prt);
 	free(film);
 	getch();
 	return 0;
@@ -47,14 +67,25 @@ void writeDebug(Film *film, int n){
 		printf("%d %lf %d\n", film[i].cod, film[i].pre, film[i].nrv);
 }
 
-// int cod; // codul filmului
-// 	int rank;// popularitatea filmului
-// 	int nrv;// nr de vizionari ale filmului avand codul cod
-// 	double pre;/
+void writeOutputFiles(Film *film, int n, double *prt) {
+	FILE *f1, *f2;
+	int i;
+	f1 = fopen("ProbsExp.txt", "wt");
+	f2 = fopen("ProbsTheor.txt", "wt");
+    if (!f1 || !f2)
+    {perror("Eroare la deschiderea fisierului de iesire."); exit(-1);}
+	for(i=0;i<n;i++){
+		fprintf(f1, "%lf\n", film[i].pre);
+		fprintf(f2, "%lf\n", prt[i]);
+	}
+	fclose(f1);
+	fclose(f2);
+}
+
 
 double *Zipf(double alpha, int n){
 	double *pr, s, c;
-	char i;
+	int i;
 	pr = (double *)allocArrayMemory(sizeof(double), n);
 	s=0;
 	for(i=0;i<n;i++)
@@ -67,19 +98,42 @@ double *Zipf(double alpha, int n){
 	return pr;
 }
 
-double *VectRep(double *pr, int n)
+void calcVectRep(double *pr, Film *film, int n, double **pFt, double **pFexp)
 {
     int i;
-    double *rep;
-    rep = (double *)allocArrayMemory(sizeof(double), n+1);
-    rep[0]=0;
-    for(i=0;i<n;i++)
-        rep[i]=rep[i-1]+pr[i-1];
-    rep[n]=1; //suma probabilitatilor 1
-    return rep;
+    double *Ft, *Fexp;
+    Ft = (double *)allocArrayMemory(sizeof(double), n+1);
+    Fexp = (double *)allocArrayMemory(sizeof(double), n+1);
+
+    Ft[0]=0;
+    Fexp[0]=0;
+    for(i=0;i<n;i++){
+        Ft[i]=Ft[i-1]+pr[i-1];
+        Fexp[i]=Fexp[i-1]+film[i-1].pre;
+
+    }
+    Ft[n]=1;
+    Fexp[n]=1; //suma probabilitatilor 1
+    (*pFt) = Ft;
+    (*pFexp) = Fexp;
 }
 
-int SimulZipf(int n, double *F)
+void simulareVizualizari(double *F, int n, int k){
+	int i, tmp;
+	int c1, t=20000;
+	c1=0;
+
+	for(i=0;i<t;i++){
+		tmp = simulZipf(n, F);
+		if(tmp<=k)
+			c1++;
+	}
+	printf("\nc1=%d", c1);
+	printf("\nt=%d", t);
+	printf("\nc1/t=%lf", (double)c1/t);
+}
+
+int simulZipf(int n, double *F)
 {
     int L=0,R=n,m;
     double u;
@@ -111,7 +165,10 @@ void scrierePuncte(char *out, Film *film, int n)
     	{perror("Eroare la deschiderea fisierului de iesire."); exit(-1);}
     for (i=0; i<n; i++)
      	if (film[i].pre!=0) // afisam doar punctele care exista 
-     		fprintf(f,"%.6lf %.6lf\n",log((double)(film[i].rank+1)),-log(film[i].pre));
+     		fprintf(f,"%.6lf %.6lf\n", 
+     			log(film[i].rank),
+     			-log(film[i].pre)
+     			);
     fclose(f);
 }
 
@@ -151,8 +208,8 @@ void* allocArrayMemory(int sizeOfElement, int count){
 	return tmp;
 }
 
-int cmpFilm(const void * pf1,const  void * pf2)
+int cmpFilm(const void *pf1,const  void *pf2)
 {
     Film *p1=(Film *)pf1, *p2=(Film *)pf2;
-    return (p1->nrv)-(p2->nrv);
+    return (p2->nrv)-(p1->nrv);
 }
